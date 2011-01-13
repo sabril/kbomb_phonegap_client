eb3a1ecd079633564ecc4d6ab32ec9a648b2257c
Collin Schaafsma <collin@quickleft.com>
2011-01-12 23:37:47 -0700

    initial import

---
+++ iphone/demo/Classes/demoAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  demoAppDelegate.h
+//  demo
+//
+//  Created by Collin Schaafsma on 1/12/11.
+//  Copyright Quick Left 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface demoAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
